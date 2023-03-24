79717f5e8b64aec3d660b4e9fd740068e74912ef
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-24 14:23:20 +0200

    finish main

--- src/task_5/Main.java
+++ src/task_5/Main.java
@@ -2,6 +2,20 @@ package task_5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Address address1 = new Address();
+        address1.index = 11001;
+        address1.country = "Ukraine";
+        address1.city = "Kyiv";
+        address1.street = "Shevchenka";
+        address1.house = 12;
+        address1.apartment = 25;
+
+
+        System.out.println("Адреса 1: " + address1.ToString());
+
+        Address address2 = new Address(54001, "Ukraine", "Mykolaiv", "Kvitneva", 95, 104);
+
+        System.out.println("Адреса 2: " + address2.ToString());
     }
 }
